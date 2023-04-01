1aad67877cec7880d197ac7a2facc552e4667e34
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-01 15:56:52 +0530

    Layout for modal done

---
+++ app/components/Button.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button: React.FC<ButtonProps> = ({ 
+  label, 
+  onClick, 
+  disabled, 
+  outline,
+  small,
+  icon: Icon,
+}) => {
+  return ( 
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      // The className prop uses a template literal to generate a class string based on the values of the outline and small props. It also includes several default classes, as well as classes that are applied when the disabled prop is true.
+      className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        ${outline ? 'bg-white' : 'bg-rose-500'}
+        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border-[1px]' : 'border-2'}
+      `}
+    >
+      {Icon && (
+        <Icon
+          size={24}
+          className="
+            absolute
+            left-4
+            top-3
+          "
+        />
+      )}
+      {label}
+    </button>
+   );
+}
+ 
+export default Button;
