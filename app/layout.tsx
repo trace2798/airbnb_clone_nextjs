1477dc8869f571c338711088a5aec32028066906
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-01 16:34:15 +0530

    Layout for registration form done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -22,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello" isOpen/>
+          <RegisterModal/>
           <Navbar />
         </ClientOnly>
         {children}
