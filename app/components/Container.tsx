347936eb49fc1e9c3103eb8ef6392482e1a62da0
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-04-01 15:05:03 +0530

    Navbar layout added

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,11 +1,13 @@
+"use client";
+
+//interface called ContainerProps. The interface has only one property - children - which is a React node that can be rendered as a child element.
 interface ContainerProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
+//creates a functional component called Container that renders a div and any children passed to it. The component takes a single prop, children, which is of type ReactNode and is provided by the ContainerProps interface.
+//The React.FC type indicates that the component is a function component.
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">{children}</div>;
+};
 
-const Container = () => {
-  return (
-    <div>Container</div>
-  )
-}
-
-export default Container
+export default Container;
